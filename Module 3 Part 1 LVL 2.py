3314ab6684dcfc8dcf40a53868825f6179a3696d
Ivan <ciferu80@gmail.com>
2024-10-11 05:04:50 +0300

    LVL 1

--- Module 3 Part 1 LVL 2.py
+++ Module 3 Part 1 LVL 2.py
@@ -1,1 +1,12 @@
-print ("Модуль 3, Часть 1, Уровень 1")
+print ("Модуль 3, Часть 1, Уровень 2")
+while True :
+    n = int(float(input("Введите число повторений n - оно должно быть целым и быть больше 1\n")))
+    if  n < 1 :
+        print("Вы ввели  число меньше 1")
+        continue
+    break
+itr = 0
+while itr < n :
+    print("for - частный случай цикла while")
+    itr = itr + 1
+

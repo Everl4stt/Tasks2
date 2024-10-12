302eb0e5cda5998e66bf8987a2e25b61c334db65
Ivan <ciferu80@gmail.com>
2024-10-12 17:08:53 +0300

    LVL 1

--- module_3_part_3_lvl_1.py
+++ module_3_part_3_lvl_1.py
@@ -2,7 +2,19 @@ def area(a, b, c) :
     p = (a + b + c)/2
     S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
     return S
-a = float(input("Введите последовательно длины сторон треугольника через ENTER\n"))
-b = float(input())
-c = float(input())
+print ("Модуль 3, Часть 3, Уровень 1")
+while True :
+    a = float(input("Введите последовательно длины сторон треугольника через ENTER\n"))
+    if a <= 0 :
+        print("Длинна стороны не может быть меньше или равной 0")
+        continue
+    b = float(input())
+    if b <= 0 :
+        print("Длинна стороны не может быть меньше или равной 0")
+        continue
+    c = float(input())
+    if c <= 0 :
+        print("Длинна стороны не может быть меньше или равной 0")
+        continue
+    break
 print("Площадь треугольника = ", area(a, b, c))

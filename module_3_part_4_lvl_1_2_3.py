10cddc525b294ca80caa400cc705c36238373196
Ivan <ciferu80@gmail.com>
2024-10-13 19:54:56 +0300

    LVL 1

--- module_3_part_4_lvl_1_2_3.py
+++ module_3_part_4_lvl_1_2_3.py
@@ -1,22 +1,36 @@
+import json
+
 print("Модуль 3, Часть 4, Уровень 1")
 def register(login, passwd) :
-    print("reg")
+    with open("data.json", "r", encoding = "utf-8") as f:
+        data = json.load(f)
+    if login not in data.keys() :
+        data[login] = passwd
+        with open("data.json", "w", encoding = "utf-8") as f:
+            json.dump(data, f)
+        print("Регистрация прошла успешно!")
+    else :
+        print("Пользователь с таким логином уже существует")
 
-#def login(login, passwd) :
+def login(login, passwd) :
+
 
 #def isregistred(login, passwd) :
+data = {}
+with open("data.json", "w", encoding = "utf-8") as f:
+    json.dump(data, f)
 while True :
     choice = input("Выберите желаемое действие из предложенных вариантов и введите его в консоль\n\t Вход\t Регистрация\t Выход \t\n")
-    if choice == 'регистрация' or choice == 'Регистрация':
-        login = input("\033[JВведите логин для регистрации\n")
+    if choice.lower() == 'регистрация' :
+        login = input("Введите логин для регистрации\n")
         passwd = input("Введите пароль для регистрации\n")
         register(login, passwd)
         continue
-    elif choice == 'вход' or choice == 'Вход' :
-        login = input("\033[JВведите логин для входа\n")
+    elif choice.lower() == 'вход' :
+        login = input("Введите логин для входа\n")
         passwd = input("Введите пароль для входа\n")
         continue
-    elif choice == 'выход' or choice == 'Выход' :
+    elif choice.lower() == 'выход' :
         break
     else :
         print("Такой вариант отсутствует, выберите из предложенных")

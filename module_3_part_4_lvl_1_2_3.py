dda406ce733f72386707478d521a4a59e3e0af32
Ivan <ciferu80@gmail.com>
2024-10-13 20:25:30 +0300

    LVL 1

--- module_3_part_4_lvl_1_2_3.py
+++ module_3_part_4_lvl_1_2_3.py
@@ -4,9 +4,7 @@ print("Модуль 3, Часть 4, Уровень 1, 2, 3")
 def register() :
     login = input("Введите логин для регистрации\n")
     passwd = input("Введите пароль для регистрации\n")
-    with open("data.json", "r", encoding = "utf-8") as f:
-        data = json.load(f)
-    if login not in data.keys() :
+    if not (isregistred(login, passwd)) :
         data[login] = passwd
         with open("data.json", "w", encoding = "utf-8") as f:
             json.dump(data, f)
@@ -16,15 +14,14 @@ def register() :
 def login() :
     login = input("Введите логин для входа\n")
     passwd = input("Введите пароль для входа\n")
-    with open("data.json", "r", encoding="utf-8") as f:
-        data = json.load(f)
     if isregistred(login, passwd) :
         print("Вход выполнен, добро пожаловать!\n", super_secret_information)
+    else :
+        print("Пользователь с таким логином не зарегистрирован!")
 def isregistred(login, passwd) :
     with open("data.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     if login not in data.keys() :
-        print("Пользователь с таким логином не зарегистрирован")
         return False
     return True
 data = {}

a128fe137377bf454dc73c6eb482af9600c418f0
Ivan <ciferu80@gmail.com>
2024-10-15 01:59:39 +0300

    LVL 1

--- module_3_part_3_lvl_3.py
+++ module_3_part_3_lvl_3.py
@@ -1,3 +1,14 @@
-
-
-
+print("Модуль 3, Часть 3, Уровень 3")
+n = [56, 9, 99, 99, 88]
+def maxnumber(n) :
+    maxnum = ""
+    for i in range(len(n)) :
+        n[i] = str(n[i])
+    for i in range(len(n)):
+        for j in range(len(n)-1) :
+            if int(n[i] + n[j]) > int(n[j] + n[i]) :
+                n[i], n[j] = n[j], n[i]
+    for i in range(len(n)) :
+        maxnum += n[i]
+    return maxnum
+print("Максимальное число из исходного массива: ", maxnumber(n))
